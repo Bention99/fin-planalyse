7ff391591c4bf77793408004132b665bebda5a88
Bention99 <benoitwyss0@gmail.com>
2026-02-24 22:49:24 +0100

    handler implementation

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var tpl = template.Must(template.ParseFiles("static/index.html"))
+
 func main() {
 	mux := http.NewServeMux()
 
-	// Serve files under ./static at /
-	fs := http.FileServer(http.Dir("./static"))
-	mux.Handle("/", fs)
+	// Static assets (css/js/images) at /static/
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	// Homepage rendered by template
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := struct{ Name string }{Name: "Benoit"}
+		if err := tpl.Execute(w, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
 
 	addr := ":8080"
 	log.Printf("http://localhost%s\n", addr)
